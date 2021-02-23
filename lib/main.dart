4452f32b99c99fb3896fc0826514259fd250b5b3
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-02-23 17:24:25 +0900

    ログインボタン、他のSNSボタン追記

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class HomePage extends StatelessWidget {
                   Text('Widget', style: TextStyle(color: Colors.white, fontSize: 40),),
                   Text('Collection', style: TextStyle(color: Colors.white, fontSize: 40),),
                   SizedBox(height: 10,),
-                  Text('Welcome Back', style: TextStyle(color: Colors.white, fontSize: 18),),
+                  // Text('Welcome Back', style: TextStyle(color: Colors.white, fontSize: 18),),
                   // RaisedButton(
                   //   child: Text('Login'),
                   //   onPressed: () {
@@ -111,8 +111,52 @@ class HomePage extends StatelessWidget {
                           ],
                         ),
                       ),
-                      // SizedBox(height: 40,),
-                      // Text('パスワードをお忘れの方はこちら'),
+                      SizedBox(height: 40,),
+                      Text('パスワードをお忘れの方はこちら', style: TextStyle(color: Colors.grey),),
+                      SizedBox(height: 30,),
+                      Container(
+                        height: 50,
+                        margin: EdgeInsets.symmetric(horizontal: 50),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.blue,
+                        ),
+                        child: Center(
+                          child: Text('ログイン', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                      SizedBox(height: 50,),
+                      Text('他のSNSでログイン', style: TextStyle(color: Colors.grey),),
+                      SizedBox(height: 30,),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.blue[200],
+                              ),
+                              child: Center(
+                                child: Text('Facebook', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 30,),
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.black,
+                              ),
+                              child: Center(
+                                child: Text('GitHub', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
